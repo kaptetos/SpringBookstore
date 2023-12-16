363f241afb1534cc20e17cfe7ec9d3632f591d9e
Nikita <kaptetos@gmail.com>
2023-12-16 10:38:15 +0200

    start of the project

---
+++ src/test/java/com/example/springbookstore/SpringBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
