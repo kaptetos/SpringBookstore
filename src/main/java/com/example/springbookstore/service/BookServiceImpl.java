79638ee5fe6d15edb33b5c77b63ff1d6287361fc
Nikita <kaptetos@gmail.com>
2023-12-22 15:13:27 +0200

    implementation of new methods

--- src/main/java/com/example/springbookstore/service/BookServiceImpl.java
+++ src/main/java/com/example/springbookstore/service/BookServiceImpl.java
@@ -1,13 +1,17 @@
 package com.example.springbookstore.service;
 
+import com.example.springbookstore.dto.CreateBookRequestDto;
 import com.example.springbookstore.entity.Book;
 import com.example.springbookstore.repository.BookRepository;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImpl implements BookService {
+
     private final BookRepository bookRepository;
 
     @Autowired
@@ -23,5 +27,33 @@ public class BookServiceImpl implements BookService {
     @Override
     public List<Book> findAll() {
         return bookRepository.findAll();
+    }
+
+    @Override
+    public Book createBook(CreateBookRequestDto createBookRequestDto) {
+        Book book = new Book();
+        BeanUtils.copyProperties(createBookRequestDto, book);
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Book not found with id: " + id));
+    }
+
+    @Override
+    public Book updateBook(Long id, CreateBookRequestDto updateBookRequestDto) {
+        Book existingBook = bookRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Book not found with id: " + id));
+
+        BeanUtils.copyProperties(updateBookRequestDto, existingBook);
+
+        return bookRepository.save(existingBook);
+    }
+
+    @Override
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
     }
 }
