fdef528ef005df1f73dfeb557d8b87344c608816
Nikita <kaptetos@gmail.com>
2023-12-22 15:11:36 +0200

    Added field removed

--- src/main/java/com/example/springbookstore/entity/Book.java
+++ src/main/java/com/example/springbookstore/entity/Book.java
@@ -1,9 +1,7 @@
 package com.example.springbookstore.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
 @Entity
@@ -24,4 +22,63 @@ public class Book {
     private String description;
 
     private String coverImage;
+
+    @Column(name = "deleted", columnDefinition = "boolean default false")
+    private boolean deleted;
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCoverImage() {
+        return coverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
