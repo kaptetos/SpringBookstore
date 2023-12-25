2dfd276e35d6cb3ae5ead5b9f7143c40bef1310a
Nikita <kaptetos@gmail.com>
2023-12-25 11:21:55 +0200

    changed version

--- src/main/java/com/example/springbookstore/entity/Book.java
+++ src/main/java/com/example/springbookstore/entity/Book.java
@@ -1,11 +1,11 @@
 package com.example.springbookstore.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.math.BigDecimal;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 public class Book {
@@ -24,7 +24,6 @@ public class Book {
     private String description;
     private String coverImage;
 
-    // Геттеры и сеттеры
     public Long getId() {
         return id;
     }
