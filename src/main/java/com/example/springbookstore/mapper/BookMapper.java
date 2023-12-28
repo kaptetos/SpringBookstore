99d0dc142886ed415ccbbea24d3c77a4f320ba93
Nikita <kaptetos@gmail.com>
2023-12-28 13:33:18 +0200

    changed as recommended

--- src/main/java/com/example/springbookstore/mapper/BookMapper.java
+++ src/main/java/com/example/springbookstore/mapper/BookMapper.java
@@ -13,12 +13,6 @@ public interface BookMapper {
     @Mappings({
             @Mapping(target = "id", ignore = true),
             @Mapping(target = "deleted", ignore = true),
-            @Mapping(target = "title", source = "createBookRequestDto.title"),
-            @Mapping(target = "author", source = "createBookRequestDto.author"),
-            @Mapping(target = "isbn", source = "createBookRequestDto.isbn"),
-            @Mapping(target = "price", source = "createBookRequestDto.price"),
-            @Mapping(target = "description", source = "createBookRequestDto.description"),
-            @Mapping(target = "coverImage", source = "createBookRequestDto.coverImage")
     })
     Book createRequestDtoToEntity(CreateBookRequestDto createBookRequestDto);
 
