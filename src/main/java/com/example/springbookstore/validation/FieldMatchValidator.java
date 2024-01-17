dc84a555c01f54148302bf487dffdefd0b4f89d7
Nikita <kaptetos@gmail.com>
2024-01-17 13:12:16 +0200

    This class is used in the @FieldMatch annotation to determine if the values of two fields in the annotated class match

---
+++ src/main/java/com/example/springbookstore/validation/FieldMatchValidator.java
@@ -0,0 +1,38 @@
+package com.example.springbookstore.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.extern.slf4j.Slf4j;
+import java.lang.reflect.Field;
+
+@Slf4j
+public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Object> {
+
+    private String firstFieldName;
+    private String secondFieldName;
+
+    @Override
+    public void initialize(FieldMatch constraintAnnotation) {
+        firstFieldName = constraintAnnotation.first();
+        secondFieldName = constraintAnnotation.second();
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        try {
+            Object firstObj = getFieldValue(value, firstFieldName);
+            Object secondObj = getFieldValue(value, secondFieldName);
+
+            return firstObj == null && secondObj == null || firstObj != null && firstObj.equals(secondObj);
+        } catch (Exception e) {
+            log.error("Field validation error", e);
+        }
+        return true;
+    }
+
+    private Object getFieldValue(Object value, String fieldName) throws NoSuchFieldException, IllegalAccessException {
+        Field field = value.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        return field.get(value);
+    }
+}
